6f052d895f190c15e8f4dd0ce33a8fd0ead810dc
noti-us <pushappsforschools@gmail.com>
2015-03-18 18:24:02 -0500

    test

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+</Response>
 <?php
 /**
  * This section ensures that Twilio gets a response.
  */
 header('Content-type: text/xml');
 echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo '<Response></Response>'; //Place the desired response (if any) here
+echo '<Response>Hi</Response>'; //Place the desired response (if any) here
  
 /**
  * This section actually sends the email.
  */
-$to      = "pushappsforschools@mgail.com"; // Your email address
+$to      = “pushappsforschools@gmail.com”; // Your email address
 $subject = "Message from {$_REQUEST['From']} at {$_REQUEST['To']}";
 $message = "You have received a message from {$_REQUEST['From']}.
 Body: {$_REQUEST['Body']}";
 $headers = "From: webmaster@example.com"; // Who should it come from?
  
 mail($to, $subject, $message, $headers);
+?>
