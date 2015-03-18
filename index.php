538f5e04bc2121c06a3a8aee08e3aa6b2fa72abb
noti-us <pushappsforschools@gmail.com>
2015-03-18 18:19:59 -0500

    test

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-
 <?php
 /**
  * This section ensures that Twilio gets a response.
  */
 header('Content-type: text/xml');
 echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo '<Response>'Hi'</Response>'; //Place the desired response (if any) here
+echo '<Response>Hi</Response>'; //Place the desired response (if any) here
  
 /**
  * This section actually sends the email.
