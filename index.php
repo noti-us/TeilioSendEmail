56676a537b992148182ff6401b0c00d01a21c659
noti-us <pushappsforschools@gmail.com>
2015-03-18 17:31:27 -0500

    Fixed typo of bracket

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
  */
 header('Content-type: text/xml');
 echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo '<Response>/Response>'; //Place the desired response (if any) here
+echo '<Response></Response>'; //Place the desired response (if any) here
  
 /**
  * This section actually sends the email.
  */
-<$to      = “pushappsforschools@gmail.com”; // Your email address
+$to      = “pushappsforschools@gmail.com”; // Your email address
 $subject = "Message from {$_REQUEST['From']} at {$_REQUEST['To']}";
 $message = "You have received a message from {$_REQUEST['From']}.
 Body: {$_REQUEST['Body']}";
