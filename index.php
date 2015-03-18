0757424dfa96d23b38e6ac298c6eda6b11d90507
noti-us <pushappsforschools@gmail.com>
2015-03-18 18:21:56 -0500

    testing

--- index.php
+++ index.php
@@ -4,16 +4,15 @@
  */
 header('Content-type: text/xml');
 echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo '<Response>Hi</Response>'; //Place the desired response (if any) here
+echo '<Response></Response>'; //Place the desired response (if any) here
  
 /**
  * This section actually sends the email.
  */
-$to      = “pushappsforschools@gmail.com”; // Your email address
+$to      = "pushappsforschools@mgail.com"; // Your email address
 $subject = "Message from {$_REQUEST['From']} at {$_REQUEST['To']}";
 $message = "You have received a message from {$_REQUEST['From']}.
 Body: {$_REQUEST['Body']}";
 $headers = "From: webmaster@example.com"; // Who should it come from?
  
 mail($to, $subject, $message, $headers);
-?>
